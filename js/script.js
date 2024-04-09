ef922f5d23a084ab25fbc91bbb8a9e3c800bf342
seoyjn016 <ssjj0229@gmail.com>
2024-04-09 11:45:58 +0900

    4_연산자

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ let a = 3,
   c = 5,
   d = 3;
 
-console.log(a === b && a === b); // true
+console.log(!a == b); // true
 console.log(a === b); // false
 console.log(a != b); // false
-console.log(a != b); //true
-console.log(a > c); //
-console.log(a < c);
-console.log(a >= c);
-console.log(a <= c);
+console.log(a !== b); // true
+console.log(a > c); // false
+console.log(a < c); // true
+console.log(a >= c); // false
+console.log(a <= c); // true
